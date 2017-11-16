2e7cd0404b6974d46443e958ecc0251b435a785d
liangz98 <liangz98@qq.com>
2017-11-17 01:53:00 +0800

    test webhooks

---
+++ app/Admin/Extensions/Simditor.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by LiangZ.
+ * Date: 2017/11/16 15:08
+ */
+
+namespace App\Admin\Extensions;
+
+use Encore\Admin\Form\Field;
+
+class Simditor extends Field {
+    protected $view = 'admin.simditor';
+    
+    protected static $css = [
+        '/css/simditor.css',
+    ];
+    
+    protected static $js = [
+        '/js/module.js',
+        '/js/hotkeys.js',
+        '/js/uploader.js',
+        '/js/simditor.js',
+    ];
+    
+    public function render() {
+        $name = $this->formatName($this->column);
+        
+        $this->script = <<<EOT
+
+var editor = new Simditor({
+    textarea: $('textarea[name=$name]'),
+});
+
+EOT;
+        return parent::render();
+    }
+}
