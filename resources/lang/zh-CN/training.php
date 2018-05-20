b607e145f269bccb52d8fe2e15bf7364da8aea94
liangz98 <liangz98@qq.com>
2018-05-20 20:49:25 +0800

    更新多语言设置

--- resources/lang/zh-CN/training.php
+++ resources/lang/zh-CN/training.php
@@ -11,5 +11,33 @@ return [
     'location'        => '培训地点',
     'start_day'       => '开始时间',
     'end_day'         => '结束时间',
-    'registration'    => '操作',
+    'registration'    => '注册',
+
+    'company_name'    => '公司全称',
+    'company_address' => '公司地址',
+    'company_phone'   => '办公电话',
+    'company_fax'     => '办公传真',
+    'mailing_address' => '证书快递地址',
+    'nace_no'         => 'NACE会员号',
+    'name'            => '姓名',
+    'department'      => '所属部门',
+    'title'           => '职务',
+    'phone'           => '家庭电话',
+    'email'           => 'E-mail 邮箱地址',
+
+    // 酒店入住相关
+    'book_hotel'    => '代订酒店',
+    'standard_room' => '标房',
+    'deluxe_room'   => '豪华房',
+    'double_bed'    => '双床',
+    'one_bed'       => '大床',
+    'book_name'     => '入住者姓名',
+    'checkIn_day'   => '入住者时间',
+    'room_night'    => '共住房晚',
+
+    // 发票相关
+    'bill'                => '开具发票',
+    'VAT_invoice'         => '增值税普通发票',
+    'VAT_special_invoice' => '增值税专用发票',
+    'invoice_title'       => '发票抬头',
 ];
